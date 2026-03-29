c9d01eecab9dfaaa4e1bab16408159e12354f9e4
Sora Arakawa <mail@arkw.net>
2026-03-29 22:55:39 +0900

    Control music based on camera signal

--- app/src/main/java/jp/arkw/offmachine/MainActivity.java
+++ app/src/main/java/jp/arkw/offmachine/MainActivity.java
@@ -4,6 +4,9 @@ import android.Manifest;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -13,11 +16,13 @@ import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Size;
 import android.view.Surface;
 import android.view.TextureView;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -27,9 +32,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.IOException;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private String id;
     private Size size;
     private Handler handler;
@@ -37,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     protected CameraCaptureSession cameraCaptureSession;
     protected CaptureRequest captureRequest;
     protected CaptureRequest.Builder captureRequestBuilder;
+    private MediaPlayer mediaPlayer;
     private TextureView textureView;
 
     private static final int REQUEST_CAMERA_PERMISSION = 200;
@@ -51,8 +58,50 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mediaPlayer = new MediaPlayer();
+        try {
+            AssetFileDescriptor assetFileDescriptor = getAssets().openFd("audio.mp3");
+            mediaPlayer.setDataSource(assetFileDescriptor.getFileDescriptor(), assetFileDescriptor.getStartOffset(), assetFileDescriptor.getLength());
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        findViewById(R.id.button_play).setOnClickListener(this);
+        findViewById(R.id.button_stop).setOnClickListener(this);
         textureView = findViewById(R.id.texture_view);
         textureView.setSurfaceTextureListener(surfaceTextureListener);
+        Handler handler = new Handler();
+        Runnable captureTask = new Runnable() {
+            @Override
+            public void run() {
+                checkTexture();
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.post(captureTask);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_play) {
+            musicPlay();
+        } else if (v.getId() == R.id.button_stop) {
+            musicStop();
+        }
+    }
+
+    private void musicPlay() {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+        }
+    }
+
+    private void musicStop() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            mediaPlayer.seekTo(0);
+        }
     }
 
     TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
@@ -146,5 +195,29 @@ public class MainActivity extends AppCompatActivity {
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    private void checkTexture() {
+        Bitmap bitmap = textureView.getBitmap(64, 64);
+        if (bitmap == null) return;
+        boolean[] check = new boolean[4];
+        check[0] = checkPixel(bitmap, 10, 10);
+        check[1] = checkPixel(bitmap, bitmap.getWidth() - 10, 10);
+        check[2] = checkPixel(bitmap, 10, bitmap.getHeight() - 10);
+        check[3] = checkPixel(bitmap, bitmap.getWidth() - 10, bitmap.getHeight() - 10);
+        if (check[0] && check[1] && check[2] && check[3]) {
+            musicPlay();
+        } else {
+            musicStop();
+        }
+        bitmap.recycle();
+    }
+
+    private boolean checkPixel(Bitmap bitmap, int x, int y) {
+        int pixel = bitmap.getPixel(x, y);
+        int r = Color.red(pixel);
+        int g = Color.green(pixel);
+        int b = Color.blue(pixel);
+        return r > 15 && g > 15 && b > 15;
     }
 }
